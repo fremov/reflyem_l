a91612a013acc907fa044b089ce35d00f7fcf344
Frem <kor7781@gmail.com>
2023-03-25 22:33:01 +0300

    auth and reg blade

--- laravel.local/resources/views/auth/login.blade.php
+++ laravel.local/resources/views/auth/login.blade.php
@@ -1,10 +1,4 @@
-<head>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet"/>
-    <!-- MDB -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <!-- MDB -->
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.2.0/mdb.min.js"></script>
-</head>
+<x-head />
 <body>
 <div class="container-fluid d-flex h-100 justify-content-center align-items-center bg-dark">
     <div
@@ -17,7 +11,7 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="mb-3 col-md-4 col-form-label text-md-right">{{ ('E-Mail') }}</label>
+                            <label for="email" class="text-white mb-3 col-md-4 col-form-label text-md-right">{{ ('E-Mail') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
@@ -31,7 +25,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="mb-3 col-md-4 col-form-label text-md-right">{{ __('Пароль') }}</label>
+                            <label for="password" class="text-white mb-3 col-md-4 col-form-label text-md-right">{{ __('Пароль') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
