6c01833541821bc7ff29026708120808b734f37d
Frem <kor7781@gmail.com>
2023-03-27 00:43:07 +0300

    fixes

--- laravel.local/resources/views/admin/wearonTable/deleted.blade.php
+++ laravel.local/resources/views/admin/wearonTable/deleted.blade.php
@@ -2,13 +2,14 @@
 <html lang="ru">
 <x-head />
 <body>
-    <div class="container mt-5">
+<div class="container mt-5">
     <h1>Удалённые записи</h1>
     <table class="table fs-5">
         <thead>
         <tr>
-            <th>Название</th>
-            <th>Действие</th>
+            <th scope="col">Название</th>
+            <th scope="col">Действие</th>
+            <th scope="col"></th>
         </tr>
         </thead>
         <tbody>
@@ -19,14 +20,14 @@
                     <form action="{{ route('restore', $record->id) }}" method="POST">
                         @csrf
                         @method('PUT')
-                        <button type="submit" class="btn btn-success">Восставновить</button>
+                        <button type="submit" class="btn btn-success w-50">Восстановить</button>
                     </form>
                 </td>
                 <td>
                     <form action="{{ route('deletedestroy', $record->id) }}" method="POST">
                         @csrf
                         @method('DELETE')
-                        <button type="submit" class="btn btn-danger">Удалить навсегда</button>
+                        <button type="submit" class="btn btn-danger w-50">Удалить навсегда</button>
                     </form>
                 </td>
             </tr>
@@ -37,8 +38,8 @@
         <div class="alert alert-success">
             {{ session('success') }}
         </div>
-        @endif
-        <a href="{{ route('admin.database') }}" class="mt-3 d-inline-block">Вернутся назад</a>
-        </div>
+    @endif
+    <a href="{{ route('admin.database') }}" class="mt-3 d-inline-block">Вернуться назад</a>
+</div>
 </body>
 </html>
