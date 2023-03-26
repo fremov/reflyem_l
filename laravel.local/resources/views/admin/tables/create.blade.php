c7ddc01c0d6103fe0ab2d330a74531064c81087e
Frem <kor7781@gmail.com>
2023-03-26 14:55:51 +0300

    admin panel tables crud bootstrap

--- laravel.local/resources/views/admin/tables/create.blade.php
+++ laravel.local/resources/views/admin/tables/create.blade.php
@@ -1,64 +1,56 @@
-{{--<form action="{{ route('database.store') }}" method="POST">--}}
-{{--    @csrf--}}
-{{--    <input type="text" name="name" placeholder="Название">--}}
-{{--    <input type="text" name="description" placeholder="Описание">--}}
-{{--    <input type="text" name="material" placeholder="Материал">--}}
-{{--    <input type="number" name="weight" placeholder="Вес">--}}
-{{--    <input type="text" name="image" placeholder="Картинка">--}}
-{{--    <input type="text" name="location" placeholder="Местонахождение">--}}
-{{--    <input type="text" name="type" placeholder="Тип">--}}
-{{--    <button type="submit">Добавить</button>--}}
-{{--</form>--}}
+<!DOCTYPE html>
+<html lang="en">
 <x-head />
-<form action="{{ route('admin.database.store') }}" method="POST" class="form-horizontal">
-    @csrf
-    <div class="form-group">
-        <label for="name" class="col-sm-2 control-label">Название</label>
-        <div class="col-sm-10">
-            <input type="text" name="name" id="name" class="form-control" placeholder="Название">
+<body>
+
+<div class="container my-5">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Create Item</h4>
+                </div>
+                <div class="card-body">
+                    <form action="{{ route('admin.database.store') }}" method="POST">
+                        @csrf
+                        <div class="form-group">
+                            <label for="name">Name:</label>
+                            <input type="text" name="name" id="name" class="form-control" placeholder="Enter name">
+                        </div>
+                        <div class="form-group">
+                            <label for="description">Description:</label>
+                            <input type="text" name="description" id="description" class="form-control" placeholder="Enter description">
+                        </div>
+                        <div class="form-group">
+                            <label for="material">Material:</label>
+                            <input type="text" name="material" id="material" class="form-control" placeholder="Enter material">
+                        </div>
+                        <div class="form-group">
+                            <label for="weight">Weight:</label>
+                            <input type="number" name="weight" id="weight" class="form-control" placeholder="Enter weight">
+                        </div>
+                        <div class="form-group">
+                            <label for="image">Image:</label>
+                            <input type="text" name="image" id="image" class="form-control" placeholder="Enter image">
+                        </div>
+                        <div class="form-group">
+                            <label for="location">Location:</label>
+                            <input type="text" name="location" id="location" class="form-control" placeholder="Enter location">
+                        </div>
+                        <div class="form-group">
+                            <label for="type">Type:</label>
+                            <input type="text" name="type" id="type" class="form-control" placeholder="Enter type">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Add Item</button>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
-    <div class="form-group">
-        <label for="description" class="col-sm-2 control-label">Описание</label>
-        <div class="col-sm-10">
-            <input type="text" name="description" id="description" class="form-control" placeholder="Описание">
-        </div>
-    </div>
-    <div class="form-group">
-        <label for="material" class="col-sm-2 control-label">Материал</label>
-        <div class="col-sm-10">
-            <input type="text" name="material" id="material" class="form-control" placeholder="Материал">
-        </div>
-    </div>
-    <div class="form-group">
-        <label for="weight" class="col-sm-2 control-label">Вес</label>
-        <div class="col-sm-10">
-            <input type="number" name="weight" id="weight" class="form-control" placeholder="Вес">
-        </div>
-    </div>
-    <div class="form-group">
-        <label for="image" class="col-sm-2 control-label">Картинка</label>
-        <div class="col-sm-10">
-            <input type="text" name="image" id="image" class="form-control" placeholder="Картинка">
-        </div>
-    </div>
-    <div class="form-group">
-        <label for="location" class="col-sm-2 control-label">Местонахождение</label>
-        <div class="col-sm-10">
-            <input type="text" name="location" id="location" class="form-control" placeholder="Местонахождение">
-        </div>
-    </div>
-    <div class="form-group">
-        <label for="type" class="col-sm-2 control-label">Тип</label>
-        <div class="col-sm-10">
-            <input type="text" name="type" id="type" class="fo
-rm-control" placeholder="Тип">
-        </div>
-    </div>
-    <div class="form-group">
-        <div class="col-sm-offset-2 col-sm-10">
-            <button type="submit" class="btn btn-default">Добавить</button>
-        </div>
-    </div>
-    <a href="{{ route('admin.database') }}">Вернутся назад</a>
-</form>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmIaP9ZjWUksdQRVvox" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+J4nW9Eckv+2OY6F5NkZQ2KRg9W0C2" crossorigin="anonymous"></script>
+</body>
+</html>
--- laravel.local/resources/views/admin/tables/edit.blade.php
+++ laravel.local/resources/views/admin/tables/edit.blade.php
@@ -1,15 +1,60 @@
-<form action="{{ route('admin.database.update', $data->id) }}" method="POST">
-    @csrf
-    @method('PUT')
-    <input type="text" name="name" placeholder="Название" value="{{ $data->name }}">
-    <input type="text" name="description" placeholder="Описание" value="{{ $data->description }}">
-    <input type="text" name="material" placeholder="Материал" value="{{ $data->material }}">
-    <input type="number" name="weight" placeholder="Вес" value="{{ $data->weight }}">
-    <input type="file" name="image" placeholder="Картинка" value="{{ $data->image }}">
-    <input type="text" name="location" placeholder="Местонахождение" value="{{ $data->location }}">
-    <input type="text" name="type" placeholder="Тип" value="{{ $data->type }}">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Edit Data</title>
+    <!-- Bootstrap CDN link -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+    <div class="row justify-content-center mt-5">
+        <div class="col-md-6">
+            <form action="{{ route('admin.database.update', $data->id) }}" method="POST">
+                @csrf
+                @method('PUT')
+                <div class="form-group">
+                    <label for="name">Название:</label>
+                    <input type="text" name="name" id="name" class="form-control" placeholder="Название" value="{{ $data->name }}">
+                </div>
+                <div class="form-group">
+                    <label for="description">Описание:</label>
+                    <input type="text" name="description" id="description" class="form-control" placeholder="Описание" value="{{ $data->description }}">
+                </div>
+                <div class="form-group">
+                    <label for="material">Материал:</label>
+                    <input type="text" name="material" id="material" class="form-control" placeholder="Материал" value="{{ $data->material }}">
+                </div>
+                <div class="form-group">
+                    <label for="weight">Вес:</label>
+                    <input type="number" name="weight" id="weight" class="form-control" placeholder="Вес" value="{{ $data->weight }}">
+                </div>
+                <div class="form-group">
+                    <label for="image">Картинка:</label>
+                    <input type="file" name="image" id="image" class="form-control" placeholder="Картинка" value="{{ $data->image }}">
+                </div>
+                <div class="form-group">
+                    <label for="location">Местонахождение:</label>
+                    <input type="text" name="location" id="location" class="form-control" placeholder="Местонахождение" value="{{ $data->location }}">
+                </div>
+                <div class="form-group">
+                    <label for="type">Тип:</label>
+                    <input type="text" name="type" id="type" class="form-control" placeholder="Тип" value="{{ $data->type }}">
+                </div>
 
-    <button type="submit">Обновить данные</button>
-</form>
+                <button type="submit" class="btn btn-primary">Обновить данные</button>
+            </form>
 
-<a href="{{ route('admin.database') }}">Вернутся назад</a>
+            <a href="{{ route('admin.database') }}" class="mt-3 d-inline-block">Вернутся назад</a>
+        </div>
+    </div>
+</div>
+
+<!-- Bootstrap CDN scripts -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1 predisável para='' reactivate='' here='' instead)='' of='' file=""  crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1r rib ='#' crossorigin="anonymous"></script>
+</body>
+</html>
